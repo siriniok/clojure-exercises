5d5b6628f9f078c6e18acc4098ece7070f89c0e2
Vladyslav Siriniok <siriniok@gmail.com>
2016-12-05 14:21:34 +0200

    Add exercises for 7th chapter

---
+++ chapter_7/exercise_1.clj
@@ -0,0 +1,2 @@
+;;;; Use the list function, quoting, and read-string to create a list that,
+;;;; when evaluated, prints your first name and your favorite sci-fi movie.
---
+++ chapter_7/exercise_2.clj
@@ -0,0 +1,3 @@
+;;;; Create an infix function that takes a list like (1 + 3 * 4 - 5) and
+;;;; transforms it into the lists that Clojure needs in order to correctly
+;;;; evaluate the expression using operator precedence rules.
