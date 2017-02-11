69bced551c0a23ccf48bee4a9995bcc069578704
Vladyslav Siriniok <siriniok@gmail.com>
2017-02-11 12:36:16 +0200

    Solve exercises for 4th chapter

--- chapter_4/exercise_1.clj
+++ chapter_4/exercise_1.clj
@@ -12,3 +12,58 @@
 ;;;; 4. Write a function that will take your list of maps and convert it back
 ;;;; to a CSV string. You’ll need to use the clojure.string/join function.
 ;;;; Good luck, McFishwich!
+
+(ns fwpd.core)
+(def filename "chapter_4/suspects.csv")
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (map :name (filter #(>= (:glitter-index %) minimum-glitter) records)))
+
+(defn csv-serialize-row
+  [name glitter-index]
+  (str name "," glitter-index "\n"))
+
+(defn csv-serailize-table
+  [table]
+  (clojure.string/join
+    (map #(csv-serialize-row (:name %1) (:glitter-index %1)) table)))
+
+(defn append
+  [name glitter-index]
+  (spit "chapter_4/suspects.csv"
+        (csv-serialize-row name glitter-index) :append true))
+
+;; Usage:
+; (append "Bloody Pastor" 14)
+; (println (glitter-filter 3 (mapify (parse (slurp filename)))))
+(println (csv-serailize-table(mapify (parse (slurp filename)))))
