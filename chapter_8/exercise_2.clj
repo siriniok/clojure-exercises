4c566dbff22676bb0a13e51602b0d439168c6f65
Vladyslav Siriniok <siriniok@gmail.com>
2017-09-14 17:10:48 +0300

    Add exercises for 8th chapter

---
+++ chapter_8/exercise_2.clj
@@ -0,0 +1,2 @@
+;;;; You saw that `and` is implemented as a macro. Implement or as a macro.
+
---
+++ chapter_8/exercise_3.clj
@@ -0,0 +1,8 @@
+;;;; In Chapter 5 you created a series of functions (c-int, c-str, c-dex) to read
+;;;; an RPG character’s attributes. Write a macro that defines an arbitrary number
+;;;; of attribute-retrieving functions using one macro call. Here’s how you would call it:
+;;;;
+;;;; (defattrs c-int :intelligence
+;;;;           c-str :strength
+;;;;           c-dex :dexterity)
+
