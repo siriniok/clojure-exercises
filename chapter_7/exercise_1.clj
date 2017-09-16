56522992f4d66dea2186f798d83af9510abeaca9
Vladyslav Siriniok <siriniok@gmail.com>
2017-09-16 11:18:26 +0300

    Add solutions to Chpater 7

--- chapter_7/exercise_1.clj
+++ chapter_7/exercise_1.clj
@@ -1,2 +1,14 @@
 ;;;; Use the list function, quoting, and read-string to create a list that,
 ;;;; when evaluated, prints your first name and your favorite sci-fi movie.
+
+(def list-list (list println "Vlad" "Mr.Nobody"))
+
+(eval list-list)
+
+(def quoted-list '(println "Vlad" "Mr.Nobody"))
+
+(eval quoted-list)
+
+(def read-string-list (read-string "(println \"Vlad\" \"Mr.Nobody\")"))
+
+(eval read-string-list)
